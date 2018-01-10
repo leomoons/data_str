5331b7d010f0a257fbbafaa70686eac399b76218
李源浩 <liyuanhao@rihajimoshisAir.lan>
2018-01-10 13:47:43 +0800

    first complete Reverse_linked_list

---
+++ Reverse_linked_list.c
@@ -0,0 +1,80 @@
+/*Write a nonrecursive procedure to reverse a singly linked list in O(N) time using constant extra space.*/
+/*The function Reverse is supposed to return the reverse linked list of L, with a dummy header.*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+typedef struct Node *PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+struct Node {
+    ElementType Element;
+    Position Next;
+};
+
+List Read(); 
+void Print( List L ); 
+List Reverse( List L );
+
+int main()
+{
+    List L1, L2;
+    L1 = Read();
+    L2 = Reverse(L1);
+    Print(L1);
+    Print(L2);
+    return 0;
+}
+
+/* Your function will be put here */
+
+
+
+List Reverse( List L ){
+	List rtu,temp,Lold;
+	rtu=(List)malloc(sizeof(struct Node));
+	rtu->Next=NULL;
+	Lold=L->Next;
+	while(Lold){
+		temp=Lold->Next;
+		Lold->Next=rtu->Next;
+		rtu->Next=Lold;
+		Lold=temp;
+	}
+	temp=rtu;
+	Lold=L->Next;
+	while(temp){
+		Lold=temp->Next;
+		Lold=Lold->Next;
+		temp=temp->Next;
+	}
+	return rtu;
+}
+
+
+List Read(){
+	int num;
+	scanf("%d",&num);
+	List L,temp;
+	L=(List)malloc(sizeof(struct Node));
+	L->Next=NULL;
+	temp=L;
+	for(int i=0;i<num;i++){
+		temp=(List)malloc(sizeof(struct Node));
+		scanf("%d",&(temp->Next->Element));
+		temp=temp->Next;
+	}
+	return L;
+}
+
+
+
+void Print(List L){
+	L=L->Next;
+	while(L){
+		printf("%d ",L->Element);
+		L=L->Next;
+	}
+}
